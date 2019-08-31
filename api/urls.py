8aff37841c99b4cc28f7caa4ed086dba098e4dbc
Trevor Nodland <trevor.nodland@gmail.com>
2019-08-31 11:18:17 -0600

    final commit

--- api/urls.py
+++ api/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('api/(?P<version>(v1|v2))/', include('music.urls'))
+    path('api/v1/', include('music.urls'))
 ]
